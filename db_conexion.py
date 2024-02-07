50b8c382d7af4969bb235ad23c6962a963edb4c1
Luis Ramirez Acosta <lramireza616@gmail.com>
2024-02-06 22:03:26 -0600

    Conexion a la Base de Datos

---
+++ db_conexion.py
@@ -0,0 +1,21 @@
+import pymongo
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = '27017'
+MONGODB_TIMEOUT = 1000
+DATABASE_NAME = 'Proyecto'
+COLLECTION_NAME='app'
+
+URI_CONNECTION = "mongodb://" + MONGODB_HOST + ":" + MONGODB_PORT +  "/"
+
+try:
+    client = pymongo.MongoClient(URI_CONNECTION, serverSelectionTimeoutMS=MONGODB_TIMEOUT)
+    db = client[DATABASE_NAME]
+    collection = db[COLLECTION_NAME]
+    client.server_info()
+    print ('OK -- Connected to MongoDB at server %s' % (MONGODB_HOST))
+    client.close()
+except pymongo.errors.ServerSelectionTimeoutError as error:
+    print ('Error with MongoDB connection: %s' % error)
+except pymongo.errors.ConnectionFailure as error:
+    print('Could not connect to MongoDB: %s' % error)

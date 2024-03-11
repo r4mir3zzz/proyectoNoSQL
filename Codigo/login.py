7d351a8035755fde94532d4862a34cb95b6c93af
Luis Ramirez Acosta <lramireza616@gmail.com>
2024-03-11 14:50:40 -0600

    x

--- Codigo/login.py
+++ Codigo/login.py
@@ -30,7 +30,7 @@ def inicio_sesion():
         code.delete(0, 'end')
 
 
-    img = PhotoImage(file='img/logo.png')
+    img = PhotoImage(file='Codigo/img/logo.png')
     img = img.subsample(2)
     label = Label(root, image=img, bg='white')
     label.place(relx=0.3, rely=0.5, anchor=CENTER)
@@ -74,11 +74,7 @@ def inicio_sesion():
     Frame(frame, width=295,height=2,bg='black').place(x=25,y=177)
 
     Button(frame, width=39,pady=7,text='INGRESAR',bg='#57a1f8',fg='white',border=0,command=signin).place(x=35,y=204)
-    label=Label(frame,text='No tiene cuenta?',fg='black',bg='white',font=('Hatton',9))
-    label.place(x=75,y=270)
 
-    crearUser = Button(frame, width=6,text='Crear',border=0,bg='white',cursor='hand2',fg='#57a1f8')
-    crearUser.place(x=215,y=270)
 
 
     root.mainloop()
@@ -90,7 +86,7 @@ def homePage():
     home.configure(bg="#EBEBEB")
     home.resizable(False, False)
 
-    img = PhotoImage(file='img/logo.png')
+    img = PhotoImage(file='Codigo/img/logo.png')
     img = img.subsample(2)
     label = Label(home, image=img, bg='#EBEBEB')
     label.place(relx=0.3, rely=0.5, anchor=CENTER)
